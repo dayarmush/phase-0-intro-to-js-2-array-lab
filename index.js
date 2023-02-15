eed9584883b048370040ce0487421c9275f65ae9
David Yarmush <yarmushda@gmail.com>
2023-02-15 15:46:24 -0500

    complete array lab test

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = [
+    'Milo',
+    'Otis',
+    'Garfield',
+];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+};
+
+function appendCat(name) {
+    return [...cats.slice(0), name,];
+};
+
+function prependCat(name) {
+    return [name, ...cats.slice(0)];
+};
+
+function removeLastCat() {
+    return [...cats.slice(0, 2,)];
+};
+
+function removeFirstCat() {
+    return [...cats.slice(1, 3,)];
+};
+
